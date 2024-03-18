0432fac07216191556545b0e38a0f339beb3bfb1
gem-muskan-a <muskan.agrawal@geminisolutions.com>
2024-03-18 17:25:01 +0530

    Added more custom runtimes

--- Python2.7/hello.sh
+++ Python2.7/hello.sh
@@ -1,28 +1,29 @@
 function handler () {
     echo $(cat /tmp/payload)
     jq -r '.Records[0].body' < /tmp/payload > /tmp/Sample.txt
-    head -n -5 /tmp/Sample.txt > /tmp/Sample.java
-    echo $(cat /tmp/Sample.java)
+    head -n -8 /tmp/Sample.txt > /tmp/Sample.py
+    echo $(cat /tmp/Sample.py)
     echo $(cat /tmp/Sample.txt)
     # Extract values from the body
-#    userid=$(grep -oE 'candidateId:[0-9]+' /tmp/Sample.txt | cut -d ':' -f 2)
+    userid=$(grep -oE 'candidateId:[0-9]+' /tmp/Sample.txt | cut -d ':' -f 2)
     qid=$(grep -oE 'questionMasterId:[0-9]+' /tmp/Sample.txt | cut -d ':' -f 2)
-#    echo "userid is $userid"
+    echo "userid is $userid"
     echo "qid is $qid"
-    code=$(cat /tmp/Sample.java)
+    code=$(cat /tmp/Sample.py)
+    testid=$(grep -oE 'testId:[0-9]+' /tmp/Sample.txt | cut -d ':' -f 2)
+    timetoans=$(grep -oE 'timeTakenToAnswer:[0-9]+' /tmp/Sample.txt | cut -d ':' -f 2)
     testcase_timeout=$(grep -oE 'testcaseTimeout:[0-9]+' /tmp/Sample.txt | cut -d ':' -f 2)
+    is_submission=$(grep -Eo 'isSubmission:[a-zA-Z]+' /tmp/Sample.txt | cut -d ':' -f 2)
     noOfTestCases=$(grep -oE 'noOfTestCases:[0-9]+' /tmp/Sample.txt | cut -d ':' -f 2)
-    verdictId=$(grep -oE 'verdictId:[0-9]+' /tmp/Sample.txt | cut -d ':' -f 2)
     noOfTestCases=${noOfTestCases:-4}
     testcase_timeout=${testcase_timeout:-2}
-
-    if javac -d /tmp /tmp/Sample.java 2> /tmp/CompilationError.txt; then
+    if python -m py_compile /tmp/Sample.py 2> /tmp/CompilationError.txt; then
         echo "Compilation successful"
         i=1
         json="["
         while [ $i -le $noOfTestCases ]
         do
-            time -f "Time(s): %e Memory(Kb): %M " timeout 2 java -cp /tmp Sample < /mnt/efs/$qid/input$i.txt > /tmp/useroutput$i 2> /tmp/error$i
+            time -f "Time(s): %e Memory(Kb): %M " timeout 2 python /tmp/Sample.py < /mnt/efs/$qid/input$i.txt > /tmp/useroutput$i 2> /tmp/error$i
             cat /tmp/useroutput$i
             cat /mnt/efs/$qid/output$i.txt
 
@@ -56,7 +57,7 @@ function handler () {
         json=${json::-1}
         json="${json}]"
         echo "Response: $json"
-        insert_query="UPDATE code_verdict SET is_compile_success = 'true', subjective_answer = '$code', verdict = '$json' , status = 'DONE' , updated_date_time = CURRENT_TIMESTAMP WHERE code_verdict_id = '$verdictId'";
+        insert_query="INSERT INTO code_verdict(candidate_id, created_date_time, is_submission, is_compile_success, language_used, question_master_id, subjective_answer, test_id, time_taken_to_answer, verdict) VALUES ('$userid', CURRENT_TIMESTAMP, '$is_submission', 'true', 'java', '$qid', '$code', '$testid', '$timetoans', '$json')";
 
     else
         error=$(cat /tmp/CompilationError.txt)
@@ -65,8 +66,8 @@ function handler () {
         echo error
         json="[$error]"
         echo "Response: $json"
-        insert_query="UPDATE code_verdict SET is_compile_success = 'false', subjective_answer = '$code', verdict = '$json' , status = 'DONE' , updated_date_time = CURRENT_TIMESTAMP WHERE code_verdict_id = '$verdictId'";
-    fi
+        insert_query="INSERT INTO code_verdict(candidate_id, created_date_time, is_submission, is_compile_success, language_used, question_master_id, subjective_answer, test_id, time_taken_to_answer, verdict) VALUES ('$userid', CURRENT_TIMESTAMP, '$is_submission', 'false', 'java', '$qid', '$code', '$testid', '$timetoans', '$json')";
+    fi  
     echo "Connecting and Inserting into db :)"
     echo $insert_query
     PGPASSWORD=${DB_PASSWORD} psql --host=${DB_ENDPOINT} --port=${DB_PORT} --user=${DB_USERNAME} ${DB_NAME} -c "$insert_query";
